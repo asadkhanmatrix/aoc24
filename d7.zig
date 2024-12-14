9c61c7504eb80a1468f4e48f3aebba6306defce9
Asad Khan <asad104khan@gmail.com>
2024-12-15 01:00:30 +0530

    d7 part 2 slow solution

--- d7.zig
+++ d7.zig
@@ -1,9 +1,28 @@
 const std = @import("std");
 
-fn works(t: i64, c: i64, r: []i64) bool {
+fn worksPart1(t: i128, c: i128, r: []i128) bool {
     if (r.len == 0) return t == c;
     if (c > t) return false;
-    return works(t, c+r[0], r[1..]) or works(t, c*r[0], r[1..]);
+    return worksPart1(t, c + r[0], r[1..]) or worksPart1(t, c * r[0], r[1..]);
+}
+
+fn concat(a: i128, b: i128) !i128 {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+    const str_a = try std.fmt.allocPrint(allocator, "{}", .{a});
+    defer allocator.free(str_a);
+    const str_b = try std.fmt.allocPrint(allocator, "{}", .{b});
+    defer allocator.free(str_b);
+    const res = try std.mem.concat(allocator, u8, &[_][]const u8{ str_a, str_b });
+    defer allocator.free(res);
+    return try std.fmt.parseInt(i128, res, 10);
+}
+
+fn worksPart2(t: i128, c: i128, r: []i128) !bool {
+    if (r.len == 0) return t == c;
+    if (c > t) return false;
+    return (try worksPart2(t, c + r[0], r[1..])) or (try worksPart2(t, c * r[0], r[1..])) or (try worksPart2(t, try concat(c, r[0]), r[1..]));
 }
 
 pub fn main() !void {
@@ -12,7 +31,7 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
     var it = std.mem.tokenizeAny(u8, data, "\n");
-    var d = std.ArrayList([]i64).init(allocator);
+    var d = std.ArrayList([]i128).init(allocator);
     defer {
         for (d.items) |l| {
             allocator.free(l);
@@ -21,16 +40,24 @@ pub fn main() !void {
     }
     while (it.next()) |line| {
         var it2 = std.mem.tokenizeAny(u8, line, " :");
-        var l = std.ArrayList(i64).init(allocator);
+        var l = std.ArrayList(i128).init(allocator);
         defer l.deinit();
         while (it2.next()) |num| {
-            try l.append(try std.fmt.parseInt(i64, num, 10));
+            try l.append(try std.fmt.parseInt(i128, num, 10));
         }
         try d.append(try l.toOwnedSlice());
     }
-    var res: i64 = 0;
+    var res1: i128 = 0;
+    var res2: i128 = 0;
     for (d.items) |l| {
-        res += if (works(l[0], l[1], l[2..])) l[0] else 0;
+        if (worksPart1(l[0], l[1], l[2..])) {
+            res1 += l[0];
+            res2 += l[0];
+        } else if (try worksPart2(l[0], l[1], l[2..])) {
+            res2 += l[0];
+        }
+        std.debug.print("res 1: {} | res 2: {}\n", .{ res1, res2 });
     }
-    std.debug.print("part 1: {}\n", .{ res });
+    std.debug.print("part 1: {}\n", .{res1});
+    std.debug.print("part 2: {}\n", .{res2});
 }
