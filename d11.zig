bf38720c83e3ce90b39628171f88bb073847f3be
Asad Khan <asad104khan@gmail.com>
2024-12-22 17:03:36 +0530

    fix memory leak bug: why didn't GPA catch the leak

--- d11.zig
+++ d11.zig
@@ -9,7 +9,7 @@ fn digitCount(n: u128) u128 {
     return cnt;
 }
 
-fn forN(allocator: std.mem.Allocator, n: u128, s: u128, mp: *std.StringHashMap(u128)) u128 {
+fn forN(allocator: std.mem.Allocator, n: u128, s: u128, mp: *std.StringHashMap(u128), all_keys: *std.ArrayList([]const u8)) u128 {
     if (s == 0) unreachable;
     if (s == 1) {
         if (n == 0 or @mod(digitCount(n), 2) == 1) {
@@ -18,17 +18,18 @@ fn forN(allocator: std.mem.Allocator, n: u128, s: u128, mp: *std.StringHashMap(u
             return 2;
         }
     }
-    const key = std.fmt.allocPrint(allocator, "{}:{}", .{n,s}) catch unreachable;
+    const key = std.fmt.allocPrint(allocator, "{}:{}", .{ n, s }) catch unreachable;
+    all_keys.append(key) catch unreachable;
     if (mp.contains(key)) {
         return mp.get(key).?;
     }
     if (n == 0) {
-        const res = forN(allocator, 1, s - 1, mp);
+        const res = forN(allocator, 1, s - 1, mp, all_keys);
         mp.put(key, res) catch unreachable;
         return res;
     }
     if (@mod(digitCount(n), 2) == 1) {
-        const res = forN(allocator, n * 2024, s - 1, mp);
+        const res = forN(allocator, n * 2024, s - 1, mp, all_keys);
         mp.put(key, res) catch unreachable;
         return res;
     }
@@ -36,7 +37,7 @@ fn forN(allocator: std.mem.Allocator, n: u128, s: u128, mp: *std.StringHashMap(u
     defer allocator.free(num);
     const n1 = std.fmt.parseInt(u128, num[0 .. num.len / 2], 10) catch unreachable;
     const n2 = std.fmt.parseInt(u128, num[num.len / 2 ..], 10) catch unreachable;
-    const res = forN(allocator, n1, s - 1, mp) + forN(allocator, n2, s - 1, mp);
+    const res = forN(allocator, n1, s - 1, mp, all_keys) + forN(allocator, n2, s - 1, mp, all_keys);
     mp.put(key, res) catch unreachable;
     return res;
 }
@@ -54,16 +55,23 @@ pub fn main() !void {
     while (it.next()) |num| {
         try l.append(try std.fmt.parseInt(u128, num, 10));
     }
+    var all_keys = std.ArrayList([]const u8).init(allocator);
+    defer {
+        for (all_keys.items) |s| {
+            allocator.free(s);
+        }
+        all_keys.deinit();
+    }
     var mp = std.StringHashMap(u128).init(allocator);
     defer mp.deinit();
     var res: u128 = 0;
     for (l.items) |v| {
-        res += forN(allocator, v, steps1, &mp);
+        res += forN(allocator, v, steps1, &mp, &all_keys);
     }
     std.debug.print("part 1: {}\n", .{res});
     res = 0;
     for (l.items) |v| {
-        res += forN(allocator, v, steps2, &mp);
+        res += forN(allocator, v, steps2, &mp, &all_keys);
     }
     std.debug.print("part 2: {}\n", .{res});
 }
